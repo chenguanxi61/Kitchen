5202109b23a9f601c51bce40a9c044c267a1ffc0
chenguanxi61 <1923064449@qq.com>
2026-01-31 02:49:35 +0800

    完成了部分音效 扛不住了先睡觉

---
+++ Assets/Scripts/ScriptsObjects/AudioClipSO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class AudioClipSO: ScriptableObject
+{
+    public AudioClip[] chop;
+    public AudioClip[] deliveryFail;
+    public AudioClip[] deliverySuccess;
+    public AudioClip[] footSteps;
+    public AudioClip[] objectDrop;
+    public AudioClip stoveSizzle;
+    public AudioClip [] trash;
+    public AudioClip [] warning;
+    public AudioClip [] pickUp;
+    
+}
